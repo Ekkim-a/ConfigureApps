fb7d9ca794737d278a2cc192df8d950bf37e63a5
Ekkim-a <73611637+Ekkim-a@users.noreply.github.com>
2022-06-19 15:13:04 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConfigureApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() =>
+            View(new Dictionary<string, string>
+            {
+                ["Message"] = "This is Index action"
+            });
+    }
+}
