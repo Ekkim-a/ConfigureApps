f30085b6880f6c6b73669379def868ea36a900e4
Ekkim-a <73611637+Ekkim-a@users.noreply.github.com>
2022-07-30 14:51:22 +0700

    Added nugets packages, libman file for packages and package file; configured kestrel, integration iis, path to content, bootstrap

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace ConfigureApps
@@ -17,10 +19,19 @@ namespace ConfigureApps
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            new HostBuilder()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
+                    webBuilder.UseKestrel();
+                    webBuilder.UseIISIntegration();
                     webBuilder.UseStartup<Startup>();
                 });
+
+        //Host.CreateDefaultBuilder(args)
+        //    .ConfigureWebHostDefaults(webBuilder =>
+        //    {
+        //        webBuilder.UseStartup<Startup>();
+        //    });
     }
 }
